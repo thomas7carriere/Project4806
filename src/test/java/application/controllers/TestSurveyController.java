f1322eaa300536752fc8985d06d312af04616246
Dom123k <DominicKocjan@cmail.carleton.ca>
2021-03-23 17:57:52 -0400

    Update TestSurveyController.java
    Resolved magic numbers for creation of AnswerDTO in setup

--- src/test/java/application/controllers/TestSurveyController.java
+++ src/test/java/application/controllers/TestSurveyController.java
@@ -47,18 +47,18 @@ public class TestSurveyController
     @Before
     public void setUp()
     {
-        QuestionDTO q1 = new QuestionDTO();q1.setQuestionType(QuestionDTO.OPENENDED);q1.setQuestion("OpenEnded Question?");
-        QuestionDTO q2 = new QuestionDTO();q2.setQuestionType(QuestionDTO.RANGE);q2.setQuestion("Range Question?");q2.setMin(1);q2.setMax(5);
-        QuestionDTO q3 = new QuestionDTO();q3.setQuestionType(QuestionDTO.MULTIPLECHOICE);q3.setQuestion("MC Question?");Collection<String> choices = new ArrayList<>();choices.add("Choice1");choices.add("Choice2");choices.add("Choice3");q3.setChoices(choices);
+        QuestionDTO q1 = new QuestionDTO();q1.setQuestionType(QuestionDTO.OPENENDED);q1.setQuestion("OpenEnded Question?");q1.setID(1L);
+        QuestionDTO q2 = new QuestionDTO();q2.setQuestionType(QuestionDTO.RANGE);q2.setQuestion("Range Question?");q2.setMin(1);q2.setMax(5);q2.setID(2L);
+        QuestionDTO q3 = new QuestionDTO();q3.setQuestionType(QuestionDTO.MULTIPLECHOICE);q3.setQuestion("MC Question?");Collection<String> choices = new ArrayList<>();choices.add("Choice1");choices.add("Choice2");choices.add("Choice3");q3.setChoices(choices);q3.setID(3L);
         Collection<QuestionDTO> questions = new ArrayList<>();questions.add(q1);questions.add(q2);questions.add(q3);
         surveyDTO = new SurveyDTO("Survey Name", questions);
-        survey = dtoToSurvey(surveyDTO);
+        survey = dtoToSurvey(surveyDTO);survey.setId(1L);
 
         HashMap<Long,String> questionAnswers= new HashMap<>();
-        questionAnswers.put(1L,"OpenEnded Answer");
-        questionAnswers.put(2L,"4");
-        questionAnswers.put(3L,"1");
-        answerDTO = new AnswerDTO(1,questionAnswers);
+        questionAnswers.put(q1.getID(),"OpenEnded Answer");
+        questionAnswers.put(q2.getID(),"4");
+        questionAnswers.put(q3.getID(),"1");
+        answerDTO = new AnswerDTO(survey.getId(),questionAnswers);
     }
 
     @Test
